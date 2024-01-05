0fa1177520dcb90a21041085bab96d8dcaf09071
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-01-05 00:35:56 -0300

    ref: Moving utils from Utils

--- utils/NameTreating.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "strings"
-
-func NameAnimeTreating(animeName string) string {
-	loweredName := strings.ToLower(animeName)
-	spacelessName := strings.ReplaceAll(loweredName, " ", "-")
-	return spacelessName
-}
