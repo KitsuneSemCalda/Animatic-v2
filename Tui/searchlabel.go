90e09840aa45591651d62a34f9e07512658c85ec
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-01-26 03:16:08 -0300

    feat(searchlabel): replace prompt-ui to bubbletea

--- Tui/searchlabel.go
+++ Tui/searchlabel.go
@@ -3,20 +3,64 @@ package tui
 import (
 	message "animatic-v2/Message"
 
-	"github.com/manifoldco/promptui"
+	tea "github.com/charmbracelet/bubbletea"
+
+	"time"
 )
 
+type searchLabelModel struct {
+	choice string
+	err    error
+	done   bool
+}
+
+func (m *searchLabelModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m *searchLabelModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter:
+			m.done = true
+			return m, tea.Quit
+		case tea.KeyBackspace:
+			if len(m.choice) > 0 {
+				m.choice = m.choice[:len(m.choice)-1]
+			}
+		default:
+			if msg.String() != "" {
+				m.choice += msg.String()
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m *searchLabelModel) View() string {
+	return "Enter the name of the anime to be downloaded: " + m.choice
+}
+
 func GetAnimeName() string {
-	label := promptui.Prompt{
-		Label: "Enter the name of the anime to be downloaded",
+	m := &searchLabelModel{}
+	p := tea.NewProgram(m)
+	errChan := make(chan error)
+	go func() {
+		errChan <- p.Start()
+	}()
+	select {
+	case err := <-errChan:
+		if err != nil {
+			message.ErrorMessage(err.Error())
+			return ""
+		}
+	case <-time.After(time.Second * 10): // Timeout after 10 seconds
+		p.Send(tea.KeyMsg{Type: tea.KeyCtrlC}) // Send Ctrl+C to the program
 	}
-
-	animeName, err := label.Run()
-
-	if err != nil {
-		message.ErrorMessage(err.Error())
-		return ""
+	if m.done {
+		return m.choice
 	}
-
-	return animeName
+	message.ErrorMessage("Could not assert model to searchLabelModel")
+	return ""
 }
